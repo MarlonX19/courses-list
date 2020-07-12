bdbe607b98b974e671cb86f87772024799e63095
marlonx19 <marlon.englemam@gmail.com>
2020-07-12 16:12:12 -0300

    added update button

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
-import { Button } from 'react-bootstrap';
+import { Button, Modal, Row, Col, Form, FormLabel } from 'react-bootstrap';
+import swal from 'sweetalert';
 import moment from 'moment';
 
 
@@ -12,17 +13,34 @@ import api from '../../services/api';
 
 function Details() {
   const location = useLocation();
-  moment().locale('pt-br');
   const courseId = location.state.courseId;
+
+  const [show, setShow] = useState(false);
+  const [title, setTitle] = useState('');
+  const [subtitle, setSubtitle] = useState('');
+  const [desc, setDesc] = useState('');
+  const [startedAt, setStartedAt] = useState('');
+  const [isActive, setIsActive] = useState(true);
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const [courseData, setCourseData] = useState(null);
 
-  console.log(courseId)
+
+  function setStates(data) {
+    setStartedAt(data[0].startedAt);
+    setTitle(data[0].title);
+    setSubtitle(data[0].subtitle);
+    setDesc(data[0].description);
+  }
 
   useEffect(() => {
     api.get(`/courses/${courseId}`)
       .then(({ status, data }) => {
         setCourseData(data);
+        setStates(data)
       })
       .catch(error => console.log(error))
   }, [])
@@ -31,6 +49,33 @@ function Details() {
     console.log('courseData')
     console.log(courseData)
   }, [courseData])
+
+
+  function handleEdit() {
+    api.put(`/courses/${courseId}`, {
+      title,
+      subtitle,
+      startedAt,
+      description: desc,
+    })
+      .then(response => {
+        handleClose();
+        swal({
+          title: "Successo!",
+          text: "Dados alterados!",
+          icon: "success",
+        });
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error)
+        swal({
+          title: "Oops!",
+          text: "Erro ao tentar alterar dados!",
+          icon: "error",
+        });
+      })
+  }
 
   return (
     <div>
@@ -54,10 +99,54 @@ function Details() {
             <p className='course-date'>{`Iniciado em ${moment(courseData[0].startedAt).format('DD/MM/YYYY HH:MM')}`}</p>
           </div>
           <div className='option-buttons'>
-            <Button variant="info">Editar dados</Button>
+            <Button variant="info" onClick={handleShow}>Editar dados</Button>
             <Button variant="danger">Deletar curso</Button>
           </div>
         </div> : null}
+
+      {
+        courseData ?
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Editar dados do curso</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <Row className='row'>
+                  <Col>
+                    <FormLabel>Título</FormLabel>
+                    <Form.Control value={title} onChange={e => setTitle(e.target.value)} placeholder={courseData[0]?.title} required />
+                  </Col>
+                  <Col>
+                    <FormLabel>Subtítulo</FormLabel>
+                    <Form.Control value={subtitle} onChange={e => setSubtitle(e.target.value)} placeholder={courseData[0]?.subtitle} required />
+                  </Col>
+                </Row>
+                <Row className='row'>
+                  <Col>
+                    <FormLabel>Data de início</FormLabel>
+                    <Form.Control type="datetime-local" value={startedAt.substring(0, 16)} onChange={e => setStartedAt(e.target.value)} />
+                  </Col>
+                </Row>
+                <Row className='row'>
+                  <Col>
+                    <FormLabel>Descrição do curso</FormLabel>
+                    <Form.Control as="textarea" rows="3" value={desc} onChange={e => setDesc(e.target.value)} placeholder={courseData[0]?.description} required />
+                  </Col>
+                </Row>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancelar
+          </Button>
+              <Button variant="primary" onClick={handleEdit}>
+                Salvar alterações
+          </Button>
+            </Modal.Footer>
+          </Modal> : null
+      }
+
 
     </div>
   )
